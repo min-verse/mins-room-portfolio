9688821e35d87a51758da0a7f2341ad733d90389
Min Tu <biffboffkev@gmail.com>
2025-01-28 21:23:40 -0500

    updating content_options

--- src/content_option.js
+++ src/content_option.js
@@ -16,69 +16,75 @@ const introdata = {
     description3: 'Additional Technologies: Docker, Jenkins, Kubernetes, AWS (Amazon Web Services), Argo CD',
     description4: 'Please click below to see my Experiences, learn more About Me, or feel free to Contact Me by using the links on the side panel',
     main_img_url:"https://i.imgur.com/s2Jek8X.png",
-    contact_url_arr: ['https://i.imgur.com/SMcOE1J.jpeg', 'https://i.imgur.com/mdQMUse.jpeg']
+    contact_url_arr: ['https://i.imgur.com/SMcOE1J.jpeg', 'https://i.imgur.com/mdQMUse.jpeg', 'https://i.imgur.com/uSYwh5F.jpeg']
 };
 
 const dataabout = {
     title: "Overview",
-    aboutme: `My name is Kevin Tu, and I'm a Software Engineer with professional Frontend, Backend, and Fullstack experience with some contributions to configuring CI/CD Pipelines and DevOps. As someone who likes taking the initiative, I've grown more and more invested in changing the world through tech. I take pride in standing up applications that are scalable, maintainable, and testable.`,
-    aboutme2:`I'm enthusiastic about jumping onto new interesting projects, creating third-party API integrations, contributing to building out new features, and leading architecting on microservices to make business logic and applications more responsive and scalable. In addition to taking charge of contructing backend systems and flow along with optimizing parts of the DevOps / CI/CD pipeline, I always am enthusiastic about building out responsive user-friendly UIs to maximize UX and SEO (Next.js, Remix.run).`,
-    aboutme3:`Since then, I continue to combine my experiences across the stack to become a competent and robust Software Engineer who can jump onto any project with passion and drive. I enjoy creating intelligent solutions with a focus on understanding a tight feedback loop, end user experience, and scaling for rapid growth.`
+    aboutme: `My name is Kevin Tu, and I'm a Software Engineer with professional Backend, Frontend, and Fullstack experience with some contributions to configuring CI/CD Pipelines and DevOps. As someone who likes taking the initiative, I've grown more and more invested in changing the world through tech. I take pride in standing up applications that are scalable, maintainable, and testable.`,
+    aboutme2:`I'm enthusiastic about jumping onto new interesting projects, creating third-party API integrations, contributing to building out new features, and leading discussions on microservices architecture to make business logic and applications more responsive and scalable. In addition to taking charge of constructing backend systems and flow along with optimizing parts of the DevOps / CI/CD pipeline, I'm always enthusiastic about building out responsive UIs to maximize UX and SEO (Next.js, Remix.run).`,
+    aboutme3:`Even now, I continue to combine my experiences across the stack to become a competent and robust Software Engineer who can jump onto any project with passion and drive. I enjoy creating intelligent solutions with a focus on understanding a tight feedback loop, end user experience, and scaling for rapid growth.`
 };
 
 const worktimeline = [
     {
         jobtitle: "Senior Backend Engineer",
         where: "Boll & Branch",
-        desc:"Integrated new technologies, automation, and dashboards with existing web apps, ensuring rapid and consistent enhancements",
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Boll_and_Branch_Logo_Wordmark.jpg/1200px-Boll_and_Branch_Logo_Wordmark.jpg',
+        desc:"As a Software Engineer on the Backend team, I mainly oversaw large change management with warehousing systems, modularizing existing integrations, creating scalable microsrevice solutions, and integrating 3rd party APIs related to e-commerce such as Loop, Stripe, Shopify, and more.",
         date: "2023-Present",
         skills: ['Ruby on Rails', 'Python', 'Node.js', 'Express.js', 'MySQL', 'Docker', 'Kubernetes', 'Jenkins', 'AWS', 'React.js', 'Remix.run', 'JavaScript', 'TypeScript', 'Redis', 'Sidekiq'],
         bullets: [
             'Lead transformation of legacy Rails monolith codebase by identifying areas of inefficiency, eliminating 80% of deprecated code, and proactively building out and spinning up scalable, resilient microservices to protect key business logic flows and integrations',
-            'Develop comprehensive fullstack solutions for a dozens of cross-functional needs such as responsive frontend analytics dashboards, warehouse and shipping status integrations and dashboard, and a small pub/sub microservice for key e-commerce storefront events',
+            'Develop comprehensive fullstack solutions for dozens of cross-functional needs such as responsive frontend analytics dashboards, warehouse and shipping status integrations and dashboard, and a small pub/sub microservice for key e-commerce storefront events',
             'Implement seamless integration with multiple critical third-party APIs for e-commerce and payments including Stripe, Loop, Shopify, various warehousing systems to increase fulfillment efficiency by over 40%'
         ]
     },
     {
         jobtitle: "Fullstack Software Engineer",
         where: "Flatiron School",
-        desc:"Integrated new technologies, automation, and dashboards with existing web apps, ensuring rapid and consistent enhancements",
+        logo: "https://coursereport-production.imgix.net/uploads/school/logo/8/original/flatironschool.png?w=72&h=72&fit=clamp&dpr=1&q=75",
+        desc:"While at Flatiron, I also took on fullstack development duties principally dedicated to migrating part of the Flatiron website to Next.js / TypeScript from React.js / JavaScript.",
         date: "2022-2023",
-        skills: ['Next.js', 'React.js', 'JavaScript', 'TypeScript', 'Ruby on Rails', 'Django'],
+        skills: ['Next.js', 'React.js', 'JavaScript', 'TypeScript', 'Ruby on Rails', 'Django', 'Python', 'Ruby'],
         bullets: [
-            'Designed and wrote out a litany of React.js Components for the website',
-            'Converted core main website pages from React.js to Next.js to optimize SEO'
+            'Built out React.js Component Library for the website',
+            'Converted core website pages from React.js to Next.js to optimize SEO',
+            'Abstracted certain authentication steps from extenal API into NextAuth'
         ]
     },
     {
         jobtitle: "Applications Developer",
         where: "JPMorgan Chase & Co.",
-        desc:"Integrated new technologies, automation, and dashboards with existing web apps, ensuring rapid and consistent enhancements",
+        logo: "https://pbs.twimg.com/profile_images/1605200105799401474/9cb5qdVk_400x400.png",
+        desc:"In JPMorgan's Global Investment Bank, I drove important business decisions regarding bank performance on ECM, DCM, and M&A deals as an Applications Developer ",
         date: "2021-2022",
         skills: ['Django', 'Python', 'Celery', 'Tableau', 'Alteryx'],
         bullets: [
-            'Convert outdated pipeline feeding Investment Bank data on ECM, DCM, and M&A deals into an automated pipeline using Tableau / Alteryx for more accurate real-time reporting to inform critical global investment bank deals',
-            'holder'
+            'Convert outdated polling data pipeline feeding Investment Bank data on ECM, DCM, and M&A deals into an automated pipeline using Alteryx for more accurate real-time reporting to inform critical global investment bank deals',
+            'Designed interactive, responsive, and filterable Tableau data visualization dashboards for financial reporting and computed risk models for investment bank in compliance with federal regulatory standards'
         ]
     },
     {
         jobtitle: "Software Development Product Manager",
         where: "JPMorgan Chase & Co.",
-        desc:"Managed app development according to the AGILE method, wrote stories, schedule sprints, and provided rigorous QA testing for UI/UX",
+        logo: "https://pbs.twimg.com/profile_images/1605200105799401474/9cb5qdVk_400x400.png",
+        desc:"During my time as a Product Manager, I became a Developer Advocate as well, and I worked across all lines of business in the bank to implement important features into our internal financial controls inventory tool. I also led cross-functional SCRUM meetings and roadmapped out sprints, product goals, and expectations.",
         date: "2020-2021",
         bullets: [
-            'managing',
-            'product'
+            'Optimized A/B Testing practices to enhance features before release resulting in a 70% improvement in user satisfaction scores and 50+ UI/UX enhancements to internal applications and systems',
+            'Led User Acceptance Testing (UAT) according to acceptance criteria and performed careful QA Testing before providing final sign-off before pushing any feature into production'
         ]
     },
     {
         jobtitle: "Xfinity Mobile Marketing Analyst",
         where: "Comcast NBCUniversal",
-        desc:"Analyzed and aggregated market trends and leveraged my research to help open over 65+ new retail locations for Comcast in this fast-paced, start-up environment",
+        logo: "https://www.xfinity.com/mobile-static/xfinitymobile-logo.jpg",
+        desc: "On the Xfinity Mobile Marketing Team, I was at the forefront of a new technology product offering from Comcast NBCUniversal and used my initiative to jump onto a project to revitalize our lead generation and marketing funnel software systems as well as bring Xfinity Mobile to more retail channels.",
         date: "2019",
         bullets: [
-            'marketing',
-            'analytics'
+            'Built out lead generation application across regional Xfinity call centers to drive conversion and upsell rates for Xfinity Mobile 80% and to track customer funnel',
+            'Analyzed and aggregated market trends and leveraged my research to help open over 65+ new retail locations for Comcast in this fast-paced, start-up environment',
         ]
     },
 ];
@@ -160,9 +166,10 @@ const skills2 = [
     }
 ];
 
-const services = [{
+const services = [
+    {
         title: "Full-Stack App Development",
-        description: "From the front-end to the back-end, I am well-versed in full-stack app development. Ready to hop on any project and quick to learn!",
+        description: "From the front-end to the back-end, even to SQL / NoSQL databases and CI/CD Pipelines, I am well-versed in full-stack app development. Ready to hop on any project and quick to learn!",
     },
     {
         title: "UI & UX Design",
@@ -196,8 +203,7 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "ktu2020@hotmail.com",
-    description: "Please contact me for any business inquiries. Glad to be of service!",
+    description: "Please contact me for any business inquiries, links on the sidebar. Glad to be of service!",
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
@@ -205,11 +211,8 @@ const contactConfig = {
 
 const socialprofils = {
     github: "https://github.com/min-verse",
-    facebook: "https://facebook.com",
     linkedin: "https://www.linkedin.com/in/kevin-tu-305805131/",
     twitter: "https://twitter.com/min_verse",
-    youtube: "https://youtube.com",
-    twitch: "https://twitch.com",
 };
 
 export {
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Container, Row, Col, NavItem } from "react-bootstrap";
-import { worktimeline, skills, dataportfolio, meta } from "../../content_option";
+import { Container, Row, Col } from "react-bootstrap";
+import { worktimeline, meta } from "../../content_option";
 
 export const Portfolio = () => {
   return (
@@ -17,99 +17,56 @@ export const Portfolio = () => {
           <Col lg="8">
             <h1 className="display-4 mb-4"> Professional Experiences </h1>{" "}
             <p className="subtitle-portfolio">A timeline of my professional experiences </p>
-            {/* <hr className="t_border my-4 ml-0 text-left" /> */}
           </Col>
         </Row>
-        {/* <Row className=" sec_sp">
-              <Col lg="5">
-                <h3 className="color_sec py-4">Relevant Professional History</h3>
-              </Col>
-              <Col lg="7">
-                <table className="table caption-top">
-                  <tbody>
-                    {worktimeline.map((data, i) => {
-                      return (
-                        <tr key={i}>
-                            <th scope="row">{data.jobtitle}</th>
-                            <td>{data.where}</td>
-                            <td>{data.desc}</td>
-                          <td>{data.date}</td>
-                        </tr>
-                      );
-                    })}
-                   </tbody>
-                </table>
-            </Col>
-        </Row> */}
         <Row>
-        <table className="table caption-top">
-                  <tbody>
-                    {worktimeline.map((data, i) => {
-                      return (
-                        <tr key={i}>
-                            <th scope="row">{data.jobtitle}</th>
-                            <td>{data.where}</td>
-                            <td>{data.desc}
-                              { 
-                                data.skills &&
-                                <>
-                                  <hr className="t_border my-4 ml-0 text-left" />
-                                  <p><em>Technologies:</em> {data.skills.join(', ')}</p>
-                                </>
-                              }
-                              <hr className="t_border my-4 ml-0 text-left" />
-                              {
-                                data.bullets && 
-                                  <ul style={{listStyle: 'inside'}}>
-                                    {
-                                      data.bullets.map((item, j)=>{
-                                        return (
-                                          <li key={j}>{item}</li>
-                                        )
-                                      })
-                                    }
-                                  </ul>
-                              }
-                            </td>
-                            <td>{data.date}</td>
-                        </tr>
-                      );
-                    })}
-                   </tbody>
-                </table>
+          <table className="table caption-top">
+                    <tbody>
+                      {worktimeline.map((data, i) => {
+                        return (
+                          <tr key={i}>
+                              <th scope="row">{data.jobtitle}</th>
+                              <td>
+                                <em>{data.where}</em>
+                                <div>
+                                  <img
+                                    width={100}
+                                    src={data.logo}
+                                    style={{borderRadius:20}}
+                                    alt={`${data.jobtitle} @ ${data.where}`}
+                                    className="mt-2"
+                                  />
+                                </div>
+                              </td>
+                              <td>{data.desc}
+                                { 
+                                  data.skills &&
+                                  <>
+                                    <hr className="t_border my-4 ml-0 text-left" />
+                                    <p><em>Technologies:</em> {data.skills.join(', ')}</p>
+                                  </>
+                                }
+                                <hr className="t_border my-4 ml-0 text-left" />
+                                {
+                                  data.bullets && 
+                                    <ul style={{listStyle: 'inside'}}>
+                                      {
+                                        data.bullets.map((item, j)=>{
+                                          return (
+                                            <li key={j} className="mb-3">{item}</li>
+                                          )
+                                        })
+                                      }
+                                    </ul>
+                                }
+                              </td>
+                              <td>{data.date}</td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
         </Row>
-        {/* <Col lg="7">
-            {skills.map((data, i) => {
-              return (
-                <div key={i}>
-                  <h3 className="progress-title">{data.name}</h3>
-                  <div className="progress">
-                    <div
-                      className="progress-bar"
-                      style={{
-                        width: `${data.value}%`,
-                      }}
-                    >
-                      <div className="progress-value">{data.value}%</div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </Col> */}
-        {/* <div className="mb-5 po_items_ho">
-          {dataportfolio.map((data, i) => {
-            return (
-              <div key={i} className="po_item">
-                <img src={data.img} alt="" />
-                <div className="content">
-                  <p>{data.desctiption}</p>
-                  <a href={data.link}>view project</a>
-                </div>
-              </div>
-            );
-          })}
-        </div> */}
       </Container>
     </HelmetProvider>
   );
